80d5eaf1f58d3dd321a4a9c0b6474ac43ed22b75
Abhijith N <nasreekar@gmail.com>
2024-08-20 23:40:05 +1000

    Add internet permissions and add app icon

--- Abhi-AnimeX/app/src/main/java/com/abhijith/animex/MainActivity.kt
+++ Abhi-AnimeX/app/src/main/java/com/abhijith/animex/MainActivity.kt
@@ -38,7 +38,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 300, heightDp = 500)
 @Composable
 fun GreetingPreview() {
     AnimeXTheme {
