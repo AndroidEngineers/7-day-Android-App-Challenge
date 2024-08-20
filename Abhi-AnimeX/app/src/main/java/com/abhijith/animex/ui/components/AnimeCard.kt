62cb4147a917e2e8ae1a9c8c6bec8e1eb4f22e14
Abhijith N <nasreekar@gmail.com>
2024-08-21 00:59:16 +1000

    update anime card to show dummy image using coil async image

--- Abhi-AnimeX/app/src/main/java/com/abhijith/animex/ui/components/AnimeCard.kt
+++ Abhi-AnimeX/app/src/main/java/com/abhijith/animex/ui/components/AnimeCard.kt
@@ -1,15 +1,22 @@
 package com.abhijith.animex.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import coil.compose.SubcomposeAsyncImage
+import coil.request.ImageRequest
 
 @Composable
 fun AnimeCard(name: String, modifier: Modifier = Modifier) {
@@ -17,14 +24,24 @@ fun AnimeCard(name: String, modifier: Modifier = Modifier) {
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-            ) {
-                Text(text = "Hello, ")
-                Text(text = name)
-            }
+        Column(
+            modifier = Modifier.fillMaxSize(1F),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            SubcomposeAsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data("https://cdn.myanimelist.net/images/anime/1006/143302.jpg")
+                    .crossfade(true)
+                    .build(),
+                loading = {
+                    CircularProgressIndicator()
+                },
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
+            Text(text = "Hello, ")
+            Text(text = name)
             ElevatedButton(
                 onClick = { }
             ) {
