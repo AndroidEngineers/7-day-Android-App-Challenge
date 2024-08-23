97595f363f71982750a4bb35194a9d95ecface62
Abhijith N <nasreekar@gmail.com>
2024-08-23 17:59:16 +1000

    Create a domain layer and create AnimeItem object along with a mapper so that data classes can be mapped when needed

---
+++ Abhi-AnimeX/app/src/main/java/com/abhijith/animex/domain/mapper/AnimeMapper.kt
@@ -0,0 +1,22 @@
+package com.abhijith.animex.domain.mapper
+
+import com.abhijith.animex.data.model.AnimeEntity
+import com.abhijith.animex.domain.model.AnimeItem
+
+object AnimeMapper {
+    fun AnimeEntity.toDomain(): AnimeItem {
+        return AnimeItem(
+            title = title,
+            source = source,
+            year = year,
+            japaneseName = japaneseName,
+            rating = rating,
+            rank = rank,
+            score = score,
+            synopsis = synopsis,
+            genres = genres,
+            imageUrl = imageUrl,
+            youtubeUrl = youtubeUrl
+        )
+    }
+}
---
+++ Abhi-AnimeX/app/src/main/java/com/abhijith/animex/domain/model/AnimeItem.kt
@@ -0,0 +1,15 @@
+package com.abhijith.animex.domain.model
+
+data class AnimeItem(
+    val title: String,
+    val source: String,
+    val year: String,
+    val japaneseName: String,
+    val rating: String,
+    val rank: String,
+    val score: String,
+    val synopsis: String,
+    val genres: List<String>,
+    val imageUrl: String,
+    val youtubeUrl: String
+)
