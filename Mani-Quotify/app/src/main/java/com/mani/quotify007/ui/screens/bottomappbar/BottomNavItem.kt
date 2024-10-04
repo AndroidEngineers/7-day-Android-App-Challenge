d24116049fb68b9ff45df9fe4fbf45f260f9f54b
itmani1990 <itmani1990@gmail.com>
2024-10-04 15:44:12 -0500

    handled select/unselect behavior of BottomAppBar

--- Mani-Quotify/app/src/main/java/com/mani/quotify007/ui/screens/bottomappbar/BottomNavItem.kt
+++ Mani-Quotify/app/src/main/java/com/mani/quotify007/ui/screens/bottomappbar/BottomNavItem.kt
@@ -4,10 +4,13 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.ui.graphics.vector.ImageVector
 
-sealed class BottomNavItem(var title: String, var icon: ImageVector, var route: String) {
-    data object Home : BottomNavItem("Home", Icons.Default.Home, "home")
-    data object Search : BottomNavItem("Search", Icons.Default.Search, "search")
-    data object Favorites : BottomNavItem("Favorites", Icons.Default.Favorite, "favorites")
+sealed class BottomNavItem(val title: String, val selectedIcon: ImageVector, val unselectedIcon: ImageVector, val route: String) {
+    data object Home : BottomNavItem("Home", Icons.Filled.Home, Icons.Outlined.Home, "home")
+    data object Search : BottomNavItem("Search", Icons.Filled.Search, Icons.Outlined.Search, "search")
+    data object Favorites : BottomNavItem("Favorites", Icons.Filled.Favorite, Icons.Outlined.Favorite, "favorites")
 }
