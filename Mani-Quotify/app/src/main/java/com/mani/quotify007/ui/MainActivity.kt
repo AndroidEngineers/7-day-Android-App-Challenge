03947b6a07b5f1c165cd6feb184b50b26498ce20
itmani1990 <itmani1990@gmail.com>
2024-10-05 12:20:23 -0500

    Feature added - comments addressed

--- Mani-Quotify/app/src/main/java/com/mani/quotify007/ui/MainActivity.kt
+++ Mani-Quotify/app/src/main/java/com/mani/quotify007/ui/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.mani.quotify007.ui.navigation.MainScreen
-import com.mani.quotify007.ui.screens.utils.ClipboardUtil
-import com.mani.quotify007.ui.screens.utils.ShareUtil
+import com.mani.quotify007.ui.screens.utils.onCopyText
+import com.mani.quotify007.ui.screens.utils.shareQuote
 import com.mani.quotify007.ui.theme.QuotifyAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,8 +14,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuotifyAppTheme {
                 MainScreen(
-                    onCopyText = { quote -> ClipboardUtil.onCopyText(this, quote) },
-                    onShareClick = { quote -> ShareUtil.shareQuote(this, quote) }
+                    onCopyText = { quote -> onCopyText(this, quote) },
+                    onShareClick = { quote -> shareQuote(this, quote) }
                 )
             }
         }
--- Mani-Quotify/app/src/main/java/com/mani/quotify007/ui/screens/utils/ClipboardUtil.kt
+++ Mani-Quotify/app/src/main/java/com/mani/quotify007/ui/screens/utils/ClipboardUtil.kt
@@ -6,16 +6,14 @@ import android.content.Context
 import android.widget.Toast
 import com.mani.quotify007.domain.model.Quote
 
-object ClipboardUtil {
-    fun onCopyText(context: Context, quote: Quote) {
-        val clipboardManager =
-            context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        clipboardManager.setPrimaryClip(
-            ClipData.newPlainText(
-                "quote",
-                "${quote.text} - ${quote.author}"
-            )
+fun onCopyText(context: Context, quote: Quote) {
+    val clipboardManager =
+        context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+    clipboardManager.setPrimaryClip(
+        ClipData.newPlainText(
+            "quote",
+            "${quote.text} - ${quote.author}"
         )
-        Toast.makeText(context, "Text copied to clipboard", Toast.LENGTH_SHORT).show()
-    }
+    )
+    Toast.makeText(context, "Text copied to clipboard", Toast.LENGTH_SHORT).show()
 }
