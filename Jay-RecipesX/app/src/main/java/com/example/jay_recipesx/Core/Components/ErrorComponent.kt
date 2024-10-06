4243e7aeff29af0d7afb13ad5914c190a010df65
ijay18.imumz <jay.inter@imumz.com>
2024-10-06 23:55:44 +0530

    Proper Folder structure.

---
+++ Jay-RecipesX/app/src/main/java/com/example/jay_recipesx/Core/Components/ErrorComponent.kt
@@ -0,0 +1,18 @@
+package com.example.jay_recipesx.Core.Components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ErrorComponent(modifier: Modifier = Modifier, errorMessage : String) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = errorMessage)
+    }
+}
