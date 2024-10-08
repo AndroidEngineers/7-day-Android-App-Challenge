ddd284064d348c0691f599bc6b73a9728f384092
ijay18.imumz <jay.inter@imumz.com>
2024-10-08 20:06:30 +0530

    Added API KEY to build.gradle.kts

--- Jay-RecipesX/app/build.gradle.kts
+++ Jay-RecipesX/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -20,6 +22,11 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
+
+        val properties = Properties()
+        properties.load(project.rootProject.file("local.properties").inputStream())
+
+        buildConfigField("String" , "API_KEY" , properties.getProperty("API_KEY"))
     }
 
     buildTypes {
@@ -40,6 +47,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
--- Jay-RecipesX/app/src/main/java/com/example/jay_recipesx/features/RecipeHome/Data/Remote/RecipeApi.kt
+++ Jay-RecipesX/app/src/main/java/com/example/jay_recipesx/features/RecipeHome/Data/Remote/RecipeApi.kt
@@ -1,5 +1,6 @@
 package com.example.jay_recipesx.features.RecipeHome.Data.Remote
 
+import com.example.jay_recipesx.BuildConfig
 import com.example.jay_recipesx.features.RecipeHome.Data.Models.RecipeModel
 import retrofit2.Call
 import retrofit2.http.GET
@@ -8,7 +9,7 @@ import retrofit2.http.Headers
 interface RecipeApi {
     @Headers(
         "Content-Type: application/json",
-        "x-api-key: 6b129304251e43469b1faa962b367fed"
+        "x-api-key: ${BuildConfig.API_KEY}"
     )
     @GET("/recipes/random")
     suspend fun getRandomRecipe() : RecipeModel
